316545f3dbcdb3e3ba97759500e1803d93ee5544
i-shashanksharma <shashanksharma240602005@gmail.com>
2025-11-19 23:07:52 +0530

    [Shashank] add Q3

---
+++ mathDashboard.js
@@ -0,0 +1,10 @@
+let x = 16.75;
+let rounded = Math.round(x);
+let sqrt = Math.sqrt(x);
+let power = Math.pow(x, 3);
+let random = Math.floor(Math.random() * 41) + 10;
+
+console.log(`Rounded: ${rounded}
+Square Root: ${sqrt}
+Power: ${power}
+Random (10-50): ${random}`);
