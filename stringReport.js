7108786ed84c4ec7c250a753c128bad5cf64a003
i-shashanksharma <shashanksharma240602005@gmail.com>
2025-11-19 23:06:43 +0530

    [Shashank] add Q2

---
+++ stringReport.js
@@ -0,0 +1,5 @@
+let product = " wireless headphones PRO ";
+let cleaned = product.trim().toLowerCase().split(" ").map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(" ");
+cleaned = cleaned.replace("Pro", "Pro Edition");
+console.log(cleaned);
+console.log("Length:", cleaned.length);
