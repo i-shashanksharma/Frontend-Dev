9287b30082546b1130a8226b1877a1ebaf1072ae
i-shashanksharma <shashanksharma240602005@gmail.com>
2025-11-19 23:10:59 +0530

    [Shashank] add Q6

---
+++ counter.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+function increment() {
+    function update() {
+        count++;
+        console.log(count);
+    }
+    update();
+}
+
+function decrement() {
+    function update() {
+        count--;
+        console.log(count);
+    }
+    update();
+}
+
+increment();
+increment();
+decrement();
